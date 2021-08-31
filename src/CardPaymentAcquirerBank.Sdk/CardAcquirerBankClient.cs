633474c095101ad6aff7cf0e4922fed8911a409a
Lesley Funwie <flesley10@gmail.com>
2021-08-31 14:11:05 +0100

    Send acquire payment request to acquiring bank

--- src/CardPaymentAcquirerBank.Sdk/CardAcquirerBankClient.cs
+++ src/CardPaymentAcquirerBank.Sdk/CardAcquirerBankClient.cs
@@ -11,34 +11,22 @@ namespace CardPaymentAcquirerBank.Sdk
     public class CardAcquirerBankClient : ICardAcquirerBankClient
     {
         private readonly HttpClient _httpClient;
-        private readonly CardAcquirerBankSettings _acquirerBankSettings;
 
-        private readonly string AcquireEndpoint = "acquire";
+        private const string AcquireEndpoint = "acquire";
 
         public CardAcquirerBankClient(HttpClient httpClient, IOptions<CardAcquirerBankSettings> options)
         {
             _httpClient = httpClient;
-            _acquirerBankSettings = options.Value;
 
-            _httpClient.BaseAddress = new Uri(_acquirerBankSettings.BaseUrl);
+            var acquirerBankSettings = options.Value;
+            _httpClient.BaseAddress = new Uri(acquirerBankSettings.BaseUrl);
         }
 
         public async Task<CardPaymentAcquirerResponse> AcquirePayment(CardPaymentAcquirerRequest request, CancellationToken cancellationToken)
         {
             var response = await _httpClient.PostAsJsonAsync(AcquireEndpoint, request, cancellationToken);
-
             response.EnsureSuccessStatusCode();
-
             return await response.Content.ReadFromJsonAsync<CardPaymentAcquirerResponse>(cancellationToken: cancellationToken);
-
-            //return Task.FromResult(new CardPaymentAcquirerResponse
-            //{
-            //    Name = "Card Acquirer",
-            //    Approved = true,
-            //    Status = "Authorized",
-            //    TransactionReference = Guid.NewGuid().ToString(),
-            //    PerformedOn = DateTime.UtcNow
-            //});
         }
     }
 }
